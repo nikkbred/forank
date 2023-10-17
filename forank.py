de0eda417afa5d6bebdd76bfcbaa0927fbff0753
Niklas Kjenstadbakk Brede <114137789+nikkbred@users.noreply.github.com>
2023-10-17 10:46:42 +0200

    Update forank.py

--- forank.py
+++ forank.py
@@ -26,7 +26,7 @@ with st.container():
     
                 ''')
     with kol4:
-        av_logo = Image.open('pics\av-logo.png')
+        av_logo = Image.open('av-logo.png')
         st.image(av_logo, caption=None, width=500)
 
 
